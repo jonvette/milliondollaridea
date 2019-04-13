44b418327158ab24004048729403ce6d14d3ff9c
frazzlerouser <samuel.oates1@gmail.com>
2019-04-12 22:17:51 -0700

    OwO what's this bulgie *pounces*

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace milliondollaridea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! OwO");
         }
     }
 }
